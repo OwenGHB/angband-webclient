52728ec018383ef6e775fab1347080fe416f155b
Gwilim Owen <gwilim.owen@gmail.com>
2017-08-02 12:02:22 +0100

    add rows/columns informations to matchupdate

--- lib.js
+++ lib.js
@@ -87,7 +87,8 @@ function getmatchlist(matches){
 	for (var i in matches){
 		livematches[i] = {
 			game: matches[i].game,
-			idletime: matches[i].idletime
+			idletime: matches[i].idletime,
+			dimensions:{rows:matches[i].term.rows,cols:matches[i].term.cols} 
 		};
 	}
 	return livematches;
@@ -317,7 +318,6 @@ function subscribe(user,message){
 	var spectator = user.username;
 	if (typeof(matches[player])!='undefined'&&typeof(matches[player].term)!='undefined'&&typeof(user.username)!='undefined') {
 		metasockets[player].send(JSON.stringify({eventtype: 'spectatorinfo', content: spectator+" is now watching"}));
-		metasockets[spectator].send(JSON.stringify({eventtype: 'spyglasstermsize', content: {player:player,dimensions:{matches[player].term.rows,matches[player].term.cols}}}));
 		matches[player].spectators.push(spectator);
 	}
 }
