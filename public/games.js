f6ce7e366f686beb9330686a0ce38439f8f05afd
Vadim <kaytrance@yandex.ru>
2017-08-03 13:48:36 +0000

    me red

--- public/games.js
+++ public/games.js
@@ -1,17 +1,17 @@
 var games = [
 	{name:'angband',longname:'Angband 4.1.0',desc:"The latest release of the classic dungeon exploration game inspired by the work of JRR Tolkien. Descended from Moria."},        
-	{name:'competition',longname:'Competition 208',desc:"Taregor is a Druadan ranger who has taken it upon himself to rid Beleriand of evil. The way is fraught with peril,         but he has many talents."},        
-	{name:'faangband',longname:'FAangband 1.4.4',desc:"Redesigned gameplay mechanics,         full of new features and new content,         including wilderness levels. Faithful to Tolkien. Descended from Oangband"},        
-	{name:'hellband',longname:'Hellband 0.8.7',desc:"Fantasy renaissance Italian setting,         informed by demonology and inspired by Dante. Descended from Zangband. Won't load keymaps (i.e. movement) from default pref folder,         you have to make your own."},        
+	{name:'competition',longname:'Competition 208',desc:"Taregor is a Druadan ranger who has taken it upon himself to rid Beleriand of evil. The way is fraught with peril, but he has many talents."},        
+	{name:'faangband',longname:'FAangband 1.4.4',desc:"Redesigned gameplay mechanics, full of new features and new content, including wilderness levels. Faithful to Tolkien. Descended from Oangband"},        
+	{name:'hellband',longname:'Hellband 0.8.7',desc:"Fantasy renaissance Italian setting, informed by demonology and inspired by Dante. Descended from Zangband. Won't load keymaps (i.e. movement) from default pref folder, you have to make your own."},        
 	{name:'nppangband',longname:'NPPAngband 7.1.0',desc:"Attempts to preserve classic angband gameplay but with various improvements."},        
 	{name:'nppmoria',longname:'NPPMoria 7.1.0',desc:"A recreation of Moria using the NPPAngband engine."},        
 	{name:'oangband',longname:'Oangband 1.1.0u',desc:'Opinion angband. Redesigned gameplay mechanics and extra content. Very influential'},        
-	{name:'poschengband',longname:'PosChengband 6.1.0b',desc:"One of the most popular variants,         bursting with new content. Includes wilderness. Descended from Zangband."},        
+	{name:'poschengband',longname:'PosChengband 6.1.0b',desc:"One of the most popular variants, bursting with new content. Includes wilderness. Descended from Zangband."},        
 	{name:'sangband',longname:'Sangband 1.0.0',desc:'Skills Angband.'},        
 	{name:'sil',longname:'Sil 1.3.0',desc:'Heavily redesigned gameplay mechanics to be a shorter but more tactically intricate game. Very faithful to Tolkein. Descended from NPPAngband'},        
-	{name:'steamband',longname:'Steamband 0.4.1f',desc:'Steampunk Angband. Please use your site username for your character name if playing,         or else progress will be lost.'},        
-	{name:'tome',longname:'ToME 2.3.6-ah',desc:"Full of new content,         quests and a large open world. Descended from Zangband."},        
+	{name:'steamband',longname:'Steamband 0.4.1f',desc:'Steampunk Angband. Please use your site username for your character name if playing, or else progress will be lost.'},        
+	{name:'tome',longname:'ToME 2.3.6-ah',desc:"Full of new content, quests and a large open world. Descended from Zangband."},        
 	{name:'umoria',longname:'UMoria 5.6',desc:'A restoration of the original Moria game.'},        
-	{name:'unangband',longname:'UnAngband 6.5.0a',desc:"Influential variant full of new content with new quests,         classes and locations."},        
-	{name:'zangband',longname:'ZAngband 2.7.5pre1',desc:"Massively influential variant. New races,         classes,         and an overworld."}
+	{name:'unangband',longname:'UnAngband 6.5.0a',desc:"Influential variant full of new content with new quests, classes and locations."},        
+	{name:'zangband',longname:'ZAngband 2.7.5pre1',desc:"Massively influential variant. New races, classes, and an overworld."}
 ];z
