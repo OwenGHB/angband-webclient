57ce35724c9f8e3f83e90c077b1f20cd57c32340
Vadim <kaytrance@yandex.ru>
2017-09-20 11:01:35 +0000

    and this

--- lib.js
+++ lib.js
@@ -318,8 +318,10 @@ function subscribe(user,message){
 	var player = message.player;
 	var spectator = user.username;
 	if (typeof(matches[player])!='undefined' && typeof(matches[player].term)!='undefined' && typeof(user.username)!='undefined') {
-		metasockets[player].send(JSON.stringify({eventtype: 'spectatorinfo', content: spectator + " is now watching"}));
-		matches[player].spectators.push(spectator);
+		if(metasockets[player]) {
+			metasockets[player].send(JSON.stringify({eventtype: 'spectatorinfo', content: spectator + " is now watching"}));
+			matches[player].spectators.push(spectator);
+		}
 		/* try {
 			metasockets[spectator].send(JSON.stringify({eventtype: 'gameoutputcache', content: {player:player,term:matches[player].termcache}}));
 		} catch (ex) {
